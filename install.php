3648cd3a9ec7dad35b633fcbe1e5e23c288b082a
T-Seven <info@nexpell.de>
2026-06-13 19:24:06 +0200

    Initial Release Update Plugin 1.0.4

--- install.php
+++ install.php
@@ -1,24 +1,59 @@
 <?php
 
-global $_database;
+if (!function_exists('safe_query')) {
+    die('Access denied');
+}
 
-## SYSTEM #####################################################################################################################################
+global $_database, $plugin;
 
-safe_query("
-    INSERT IGNORE INTO settings_plugins
-        (pluginID, modulname, admin_file, activate, author, website, index_link, hiddenfiles, version, path, status_display, plugin_display, widget_display, delete_display, sidebar)
+$modulname = 'masterlist';
+$version = isset($plugin['version']) ? (string)$plugin['version'] : ($version ?? '1.0.3.3');
+$pluginName = 'Masterlist';
+$pluginPath = 'includes/plugins/masterlist/';
+
+if (!function_exists('masterlist_sql')) {
+    function masterlist_sql($value): string
+    {
+        return escape((string)$value);
+    }
+}
+
+## SYSTEM #######################################################################
+
+$pluginRes = safe_query("SELECT pluginID FROM settings_plugins WHERE modulname = 'masterlist' LIMIT 1");
+if ($pluginRes && ($pluginRow = mysqli_fetch_assoc($pluginRes))) {
+    safe_query("UPDATE settings_plugins SET
+        admin_file = '',
+        activate = 1,
+        author = 'T-Seven',
+        website = 'https://www.nexpell.de',
+        index_link = 'masterlist',
+        hiddenfiles = '',
+        version = '" . masterlist_sql($version) . "',
+        path = '" . masterlist_sql($pluginPath) . "',
+        status_display = 1,
+        plugin_display = 1,
+        widget_display = 1,
+        delete_display = 1,
+        sidebar = 'deactivated'
+        WHERE pluginID = " . (int)$pluginRow['pluginID'] . "
+    ");
+} else {
+    safe_query("INSERT INTO settings_plugins
+        (modulname, admin_file, activate, author, website, index_link, hiddenfiles, version, path, status_display, plugin_display, widget_display, delete_display, sidebar)
     VALUES
-        ('', 'masterlist', '', 1, 'T-Seven', 'https://www.nexpell.de', 'masterlist', '', '1.0.3.3', 'includes/plugins/masterlist/', 1, 1, 1, 1, 'deactivated')
-");
+        ('masterlist', '', 1, 'T-Seven', 'https://www.nexpell.de', 'masterlist', '', '" . masterlist_sql($version) . "', '" . masterlist_sql($pluginPath) . "', 1, 1, 1, 1, 'deactivated')
+    ");
+}
 
 safe_query("
-    INSERT IGNORE INTO settings_plugins_lang
+    INSERT INTO settings_plugins_lang
         (content_key, language, content, modulname, updated_at)
     VALUES
         ('plugin_name_masterlist', 'de', 'Masterlist', 'masterlist', NOW()),
         ('plugin_name_masterlist', 'en', 'Masterlist', 'masterlist', NOW()),
         ('plugin_name_masterlist', 'it', 'Masterlist', 'masterlist', NOW()),
-        ('plugin_info_masterlist', 'de', 'Mit diesem Plugin koennt ihr eure Masterlist mit Slider und Seite anzeigen lassen.', 'masterlist', NOW()),
+        ('plugin_info_masterlist', 'de', 'Mit diesem Plugin könnt ihr eure Masterlist mit Slider und Seite anzeigen lassen.', 'masterlist', NOW()),
         ('plugin_info_masterlist', 'en', 'With this plugin you can display your masterlist with slider and page.', 'masterlist', NOW()),
         ('plugin_info_masterlist', 'it', 'Con questo plugin puoi visualizzare la tua masterlist con slider e pagina.', 'masterlist', NOW())
     ON DUPLICATE KEY UPDATE
@@ -28,10 +63,10 @@ safe_query("
 ");
 
 safe_query("
-    INSERT IGNORE INTO settings_plugins_installed
+    INSERT INTO settings_plugins_installed
         (name, modulname, description, version, author, url, folder, installed_date)
     VALUES
-        ('Masterlist', 'masterlist', 'With this plugin you can display your masterlist with slider and page.', '1.0.3.3', 'nexpell-team', 'https://www.nexpell.de', 'masterlist', NOW())
+        ('Masterlist', 'masterlist', 'With this plugin you can display your masterlist with slider and page.', '" . masterlist_sql($version) . "', 'nexpell-team', 'https://www.nexpell.de', 'masterlist', NOW())
     ON DUPLICATE KEY UPDATE
         name = VALUES(name),
         description = VALUES(description),
@@ -42,29 +77,38 @@ safe_query("
         installed_date = NOW()
 ");
 
-## NAVIGATION #####################################################################################################################################
+## NAVIGATION ###################################################################
 
 $snavID = 0;
 $snavRes = safe_query("
     SELECT snavID FROM navigation_website_sub
-    WHERE modulname = 'masterlist' AND url = 'index.php?site=masterlist'
+    WHERE modulname = 'masterlist'
     ORDER BY snavID ASC LIMIT 1
 ");
 if ($snavRes && ($snavRow = mysqli_fetch_assoc($snavRes))) {
-    $snavID = (int) ($snavRow['snavID'] ?? 0);
+    $snavID = (int)($snavRow['snavID'] ?? 0);
+    safe_query("
+        UPDATE navigation_website_sub SET
+            mnavID = 6,
+            url = 'index.php?site=masterlist',
+            sort = 1,
+            indropdown = 1,
+            last_modified = NOW()
+        WHERE snavID = " . $snavID . "
+    ");
 } else {
     safe_query("
-        INSERT IGNORE INTO navigation_website_sub
+        INSERT INTO navigation_website_sub
             (mnavID, modulname, url, sort, indropdown, last_modified)
         VALUES
             (6, 'masterlist', 'index.php?site=masterlist', 1, 1, NOW())
     ");
-    $snavID = (int) mysqli_insert_id($_database);
+    $snavID = (int)mysqli_insert_id($_database);
 }
 
 if ($snavID > 0) {
     safe_query("
-        INSERT IGNORE INTO navigation_website_lang
+        INSERT INTO navigation_website_lang
             (content_key, language, content, modulname, updated_at)
         VALUES
             ('nav_sub_{$snavID}', 'de', 'Masterliste', 'masterlist', NOW()),
@@ -77,9 +121,9 @@ if ($snavID > 0) {
     ");
 }
 
-#######################################################################################################################################
 safe_query("
-  INSERT IGNORE INTO user_role_admin_navi_rights (id, roleID, type, modulname)
-  VALUES ('', 1, 'link', 'masterlist')
+    INSERT IGNORE INTO user_role_admin_navi_rights
+        (id, roleID, type, modulname)
+    VALUES
+        ('', 1, 'link', 'masterlist')
 ");
-?>
--- update.php
+++ update.php
@@ -1,80 +1,18 @@
 <?php
 
-global $_database;
-
-safe_query("
-    INSERT IGNORE INTO settings_plugins
-        (pluginID, modulname, admin_file, activate, author, website, index_link, hiddenfiles, version, path, status_display, plugin_display, widget_display, delete_display, sidebar)
-    VALUES
-        ('', 'masterlist', '', 1, 'T-Seven', 'https://www.nexpell.de', 'masterlist', '', '1.0.3.3', 'includes/plugins/masterlist/', 1, 1, 1, 1, 'deactivated')
-");
-
-safe_query("
-    INSERT IGNORE INTO settings_plugins_lang
-        (content_key, language, content, modulname, updated_at)
-    VALUES
-        ('plugin_name_masterlist', 'de', 'Masterlist', 'masterlist', NOW()),
-        ('plugin_name_masterlist', 'en', 'Masterlist', 'masterlist', NOW()),
-        ('plugin_name_masterlist', 'it', 'Masterlist', 'masterlist', NOW()),
-        ('plugin_info_masterlist', 'de', 'Mit diesem Plugin koennt ihr eure Masterlist mit Slider und Seite anzeigen lassen.', 'masterlist', NOW()),
-        ('plugin_info_masterlist', 'en', 'With this plugin you can display your masterlist with slider and page.', 'masterlist', NOW()),
-        ('plugin_info_masterlist', 'it', 'Con questo plugin puoi visualizzare la tua masterlist con slider e pagina.', 'masterlist', NOW())
-    ON DUPLICATE KEY UPDATE
-        content = VALUES(content),
-        modulname = VALUES(modulname),
-        updated_at = VALUES(updated_at)
-");
-
-safe_query("
-    INSERT IGNORE INTO settings_plugins_installed
-        (name, modulname, description, version, author, url, folder, installed_date)
-    VALUES
-        ('Masterlist', 'masterlist', 'With this plugin you can display your masterlist with slider and page.', '1.0.3.3', 'nexpell-team', 'https://www.nexpell.de', 'masterlist', NOW())
-    ON DUPLICATE KEY UPDATE
-        name = VALUES(name),
-        description = VALUES(description),
-        version = VALUES(version),
-        author = VALUES(author),
-        url = VALUES(url),
-        folder = VALUES(folder),
-        installed_date = NOW()
-");
-
-$snavID = 0;
-$snavRes = safe_query("
-    SELECT snavID FROM navigation_website_sub
-    WHERE modulname = 'masterlist' AND url = 'index.php?site=masterlist'
-    ORDER BY snavID ASC LIMIT 1
-");
-if ($snavRes && ($snavRow = mysqli_fetch_assoc($snavRes))) {
-    $snavID = (int) ($snavRow['snavID'] ?? 0);
-} else {
-    safe_query("
-        INSERT IGNORE INTO navigation_website_sub
-            (mnavID, modulname, url, sort, indropdown, last_modified)
-        VALUES
-            (6, 'masterlist', 'index.php?site=masterlist', 1, 1, NOW())
-    ");
-    $snavID = (int) mysqli_insert_id($_database);
+if (!function_exists('safe_query')) {
+    die('Access denied');
 }
 
-if ($snavID > 0) {
-    safe_query("
-        INSERT IGNORE INTO navigation_website_lang
-            (content_key, language, content, modulname, updated_at)
-        VALUES
-            ('nav_sub_{$snavID}', 'de', 'Masterliste', 'masterlist', NOW()),
-            ('nav_sub_{$snavID}', 'en', 'Game Masterlist', 'masterlist', NOW()),
-            ('nav_sub_{$snavID}', 'it', 'Lista giochi', 'masterlist', NOW())
-        ON DUPLICATE KEY UPDATE
-            content = VALUES(content),
-            modulname = VALUES(modulname),
-            updated_at = VALUES(updated_at)
-    ");
-}
+global $str, $modulname, $version, $plugin;
 
-safe_query("
-  INSERT IGNORE INTO user_role_admin_navi_rights (id, roleID, type, modulname)
-  VALUES ('', 1, 'link', 'masterlist')
-");
-?>
+$modulname = 'masterlist';
+$version = isset($plugin['version']) ? (string)$plugin['version'] : ($version ?? '1.0.3.3');
+$str = 'Masterlist';
+
+echo '<div class="card"><div class="card-body">';
+echo '<h4>Update: ' . htmlspecialchars($str, ENT_QUOTES, 'UTF-8') . ' (' . htmlspecialchars($version, ENT_QUOTES, 'UTF-8') . ')</h4>';
+
+require __DIR__ . '/install.php';
+
+echo '</div></div>';
